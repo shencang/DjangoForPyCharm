3e099c18e02c21bead544074e333fbfcef0427cf
shencang <2630610009@qq.com>
2019-04-08 22:41:29 +0800

    推进

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 # Create your models here.
-
-class Qustion(models.Model):
+@python_2_unicode_compatible  # 当你想支持python2版本的时候才需要这个装饰器
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
-class Choice (models.Model):
-    question =models.ForeignKey(Qustion,on_delete=models.CASCADE)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_test = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
