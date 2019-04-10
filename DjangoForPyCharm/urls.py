51a4e9622b5938f324eea8e7869415c968eb325c
shencang <2630610009@qq.com>
2019-04-10 21:47:25 +0800

    admin完成

--- DjangoForPyCharm/urls.py
+++ DjangoForPyCharm/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('polls/',include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('my/set/', admin.site.urls),
 
 ]
