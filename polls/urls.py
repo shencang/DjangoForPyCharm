e9a64590bea35be649fb041ab0e5bb105ce9b20d
shencang <2630610009@qq.com>
2019-04-10 23:05:56 +0800

    views

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index', )
+    # ex: /polls/
+    path('', views.index, name='index', ),
+    # ex: /polls/5/
+    path('(?P<question_id>[0-9]+)/', views.detail, name='detail'),
+    # ex：/polls/5/results/
+    path('(?P<question_id>[0-9]+)/results/', views.results, name='results'),
+    # ex:/polls/5/vote/
+    path('(?P<question_id>[0-9]+)/vote/', views.vote, name='vote'),
+
 ]
